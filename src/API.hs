9cfe71c75818c86f5d7221d992e8dc4c74be50c1
Real Limoges <b.real.limoges@gmail.com>
2025-12-27 05:36:57 -0800

    build basic bones of Servant API type

--- src/API.hs
+++ src/API.hs
@@ -1,9 +1,31 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-
 module API where
 
-import Servant
-import Types
+import Control.Monad.Reader
+import Control.Monad.Except
+import Data.Text (Text)
+import Database.Redis (Connection)
+import qualified Database.PostgreSQL.Simple as PG
+import Data.Pool (Pool, withResource)
+import Servant (Handler, ServerError)
 
-type MyAPI = "predict" :> ReqBody '[JSON] Payload :> Post '[JSON] TaskResult
+-- state management
+data AppConfig = AppConfig
+  { redisConn :: Connection
+  , pgPool :: Pool PG.Connection
+  , queueName :: Text
+  }
+
+-- this is the magic of Servant. It runs on a type.
+-- it derives a bunch of stuff - this is the composition over inheritance
+newtype AppM = AppM { runAppM :: ReaderT AppConfig Handler a }
+deriving stock (Functor, Applicative, Monad, MonadIO, MonadReader
+               , AppConfig, MonadError, ServerError
+               )
+
+runApp :: AppConfig -> AppM a -> Handler a
+runApp config app = runReaderT (runAppM) config
+
+runDb :: (PG.Connection -> IO a) -> AppM a
+runDb queryAction = do
+  pool <- asks pgPool
+  liftIO $ withResource pool queryAction
